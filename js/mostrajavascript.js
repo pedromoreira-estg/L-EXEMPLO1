43377bfe52a86c0837ea3db5badb22d5d6120775
Pedro Moreira <pmoreira.estg@gmail.com>
2012-10-08 14:45:25 +0100

    scripts separados

---
+++ js/mostrajavascript.js
@@ -0,0 +1,28 @@
+/**
+ * @author Pedro Moreira
+ */
+
+			 // window.onload = function() {...}
+			 
+			 window.addEventListener("load", function () {
+			 	var fotos = document.getElementsByClassName("foto");
+			 	var f;
+			 	for(f=0; f < fotos.length; f++) {
+					fotos[f].style.display="none";
+					}
+				var legendas = document.getElementsByClassName("legenda");
+				var l;
+				for(l=0; l < legendas.length; l++) {
+					legendas[l].addEventListener("click",function () {
+						//alert("ui   " + this.innerHTML);
+						//debugger;
+						this.parentNode;
+						var afoto = this.parentNode.getElementsByClassName("foto")[0];
+						if (afoto.style.display == "block") {
+								afoto.style.display = "none";	
+						} else { 
+								afoto.style.display = "block";
+						}
+					});
+				}
+			});
